4eb89e341cc153497c95f44005908ab3c1362713
yangyi <1839041324@qq.com>
2025-11-12 01:02:50 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Hello />
+    <p>{{ sum }}</p>
+    <p>{{ greeting }}</p>
+  </div>
+</template>
+
+<script>
+import Hello from './components/Hello.vue'
+import { add, greet } from './utils.js'
+
+let sum = add(2, 3)
+let greeting = greet("Alice")
+
+export default {
+  name: 'App',
+  components: { Hello },
+  data() {
+    return {
+      sum: add(2, 3),
+      greeting: greet("Alice")
+    }
+  }
+}
+</script>
---
+++ src/components/Hello.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hello",
+  data() {
+    return {
+      message: "This is a Hello Component"
+    }
+  }
+}
+</script>
